d78fb58b59c636ff357102832158c28f4ad7bd87
kayikalvin <kayikalvin@gmail.com>
2025-02-18 09:58:51 -0500

    controller

--- api/controllers/user.conroller.js
+++ api/controllers/user.conroller.js
@@ -1,6 +1,6 @@
 /// used to hide the user logic from user.routes.js for best practices
 export const test =(req,res) =>{
     res.json({
-        message:"api is working"
+        message:"api is working "
     })
 };
