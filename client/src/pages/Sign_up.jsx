cedb0cf3b47679b2151279346153495deb63831d
kayikalvin <kayikalvin@gmail.com>
2025-02-24 07:03:00 -0500

    complete sign in page functionality

--- client/src/pages/Sign_up.jsx
+++ client/src/pages/Sign_up.jsx
@@ -75,7 +75,7 @@ export default function Sign_up() {
       </form>
       <div className='flex gap-2 mt-5'>
         <p>Have an account?</p>
-        <Link to={'/sign-in'}>
+        <Link to={'/sign_in'}>
           <span className='text-blue-700'>Sign in</span>
         </Link>
       </div>
