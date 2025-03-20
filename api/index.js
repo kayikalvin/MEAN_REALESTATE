53a0bd06a572f56c58455cacf2287cd7a8746900
kalvohh <kayikalvin.com>
2025-03-20 13:45:00 +0300

    set up google auth using firebase

--- api/index.js
+++ api/index.js
@@ -31,7 +31,7 @@ app.listen(3000,() => {
     console.log('Server is running on port 3000 .....') 
 });
 app.get('/',(req,res) => {
-    res.send('Welcome to Kayi-Listings')
+    res.send('Welcome to')
 })
 
 
